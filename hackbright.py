e72ec326fb7e1302d89e26482fc775e170b04693
Hackbright Student <info@hackbrightacademy.com>
2015-07-21 16:11:34 -0700

    Handling errors and lots of new functions

--- hackbright.py
+++ hackbright.py
@@ -18,10 +18,15 @@ def get_student_by_github(github):
         FROM Students
         WHERE github = ?
         """
+
     db_cursor.execute(QUERY, (github,))
     row = db_cursor.fetchone()
-    print "Student: %s %s\nGithub account: %s" % (
-        row[0], row[1], row[2])
+
+    if row == None:
+        print "That student is not on file."
+    else:
+        print "Student: %s %s\nGithub account: %s" % (
+            row[0], row[1], row[2])
 
 
 def make_new_student(first_name, last_name, github):
@@ -29,48 +34,79 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    QUERY = """INSERT INTO Students VALUES (?, ?, ?)"""
-    db_cursor.execute(QUERY, (first_name, last_name, github))
 
+    QUERY = """INSERT INTO Students VALUES (?, ?, ?)"""
+
+    db_cursor.execute(QUERY, (first_name, last_name, github))
     db_connection.commit()
+
     print "Successfully added student: %s %s" %(first_name, last_name)
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    QUERY = """
-    SELECT * FROM Projects WHERE title = ? """
+    
+    QUERY = """SELECT * FROM Projects WHERE title = ? """
+
     db_cursor.execute(QUERY, (title,))
     row = db_cursor.fetchone()
-    db_connection.commit()
-    print "Project ID: %d, Project title: %s, Project description: %s, Max Grade: %d" % (
+
+    if row == None:
+        print "Invalid title."
+    else:
+        print "Project ID: %d, Project title: %s, Project description: %s, Max Grade: %d" % (
         row[0], row[1], row[2], row[3]) 
 
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
+
     QUERY = """SELECT grade FROM Grades
                 WHERE student_github = ? AND project_title = ?"""
+
     db_cursor.execute(QUERY, (github, title))
     row = db_cursor.fetchone()
-    db_connection.commit()
-    print "Here's the grade for %s's %s: %s" % (github, title, row[0])
+
+    if row == None:
+        print "Invalid student or project."
+    else:
+        print "Here's the grade for %s's %s: %s" % (github, title, row[0])
 
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
+
     QUERY = """INSERT INTO Grades VALUES (?, ?, ?)"""
+
     db_cursor.execute(QUERY, (github, title, grade))
     db_connection.commit()
+
     print "Successfully added %s's grade for %s" % (github, title)
 
 def assign_project(title, max_grade, description):
     """Create a project with a given title, max grade and description of any length."""
+
     QUERY = """INSERT INTO Projects (title, max_grade, description) VALUES(?, ?, ?)"""
+
     db_cursor.execute(QUERY, (title, max_grade, description))
     db_connection.commit()
+
     print "Successfully added project: %s" % (title)
 
+def get_all_grades(github):
+    """Gets all grades for a student"""
+
+    QUERY = """SELECT grade, project_title FROM Grades WHERE student_github = ?"""
+
+    db_cursor.execute(QUERY, (github, ))
+    results = db_cursor.fetchall()
+
+    if results == []:
+        print "This github doesn't exist!"
+    else:
+        for result in results:
+            grade, title = result
+            print "Here are the grades for %s: %s, %s" % (github, title, grade)
 
 def handle_input():
     """Main loop.
@@ -87,33 +123,58 @@ def handle_input():
         args = tokens[1:]
 
         if command == "student":
-            github = args[0]
-            get_student_by_github(github)
+            if len(args) != 1:
+                print "Please enter the github handle of a student."
+            else:
+                github = args[0]
+                get_student_by_github(github)
 
         elif command == "new_student":
-            first_name, last_name, github = args   # unpack!
-            make_new_student(first_name, last_name, github)
+            if len(args) != 3:
+                print "Please enter first_name, last_name, and github."
+            else: 
+                first_name, last_name, github = args   # unpack!
+                make_new_student(first_name, last_name, github)
 
         elif command == "project_title":
-            title = args[0]
-            get_project_by_title(title)
+            if len(args) != 1:
+                print "Please enter a project title."
+            else: 
+                title = args[0]
+                get_project_by_title(title)
 
         elif command == "get_grade":
-            github = args[0]
-            title = args[1]
-            get_grade_by_github_title(github, title)
+            if len(args) != 2:
+                print "Please enter github and project title."
+            else:
+                github = args[0]
+                title = args[1]
+                get_grade_by_github_title(github, title)
 
         elif command == "assign_grade":
-            github = args[0]
-            title = args[1]
-            grade = args[2]
-            assign_grade(github, title, grade)
+            if len(args) != 3:
+                print "Please enter GitHub, project title, and grade."
+            else: 
+                github = args[0]
+                title = args[1]
+                grade = args[2]
+                assign_grade(github, title, grade)
 
         elif command == "assign_project":
-            title = args[0]
-            max_grade = args[1]
-            description = " ".join(args[2:])
-            assign_project(title, max_grade, description)
+            if len(args) < 3:
+                print "Please enter title, max grade and description."
+            else:
+                title = args[0]
+                max_grade = args[1]
+                description = " ".join(args[2:])
+                assign_project(title, max_grade, description)
+
+        elif command == "get_grades":
+            if len(args) != 1:
+                print "Please enter a student github handle."
+            else:
+                github = args[0]
+                get_all_grades(github)
 
 
 if __name__ == "__main__":
